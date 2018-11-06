200e5ba038f066642d8613510274d987a220882f
Vladislav Starikov <vlad.starikov.88@gmail.com>
2018-11-06 16:52:15 +0300

    Добавлена корзина в store

--- front-end/src/store/cart.js
+++ front-end/src/store/cart.js
@@ -1,8 +1,26 @@
+import lodash from 'lodash'
 const cart = {
   state: {
-
+    cart: []
   },
   mutations: {
-    
+    addToCartById(state, [phone_id, amount]) {
+      const record = lodash.find(state.cart, { phone_id })
+      if(record) {
+        record.amount += amount;
+      } else {
+        state.cart.push({
+          phone_id,
+          amount,
+        })
+      }
+    }
+  },
+  actions: {
+    addToCartById({commit, }, [phone_id, amount]) {
+      commit('addToCartById', [phone_id, amount])
+    }
   }
 }
+
+export default cart
