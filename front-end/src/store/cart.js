94300283c2c1d91f743154522f2cf4d46b609afe
Vladislav Starikov <vlad.starikov.88@gmail.com>
2018-11-06 19:26:31 +0300

    Исправление ошибки

--- front-end/src/store/cart.js
+++ front-end/src/store/cart.js
@@ -18,11 +18,11 @@ const cart = {
     removeFromCartById(state, phone_id) {
       lodash.remove(state.cart, {phone_id})
     },
-    changeAmountFromCartById(state, [phone_id, newAmount]) {
+    changeAmountFromCartById(state, [phone_id, new_amount]) {
       const current = lodash.find(state.cart, {phone_id}) 
 
       if (current) {
-        current.amount = newAmount
+        current.amount = new_amount
       }
     }
   },
@@ -33,8 +33,8 @@ const cart = {
     removeFromCartById({commit}, phone_id) {
       commit('removeFromCartById', phone_id)
     },
-    changeAmountFromCartById({commit}, [phone_id, newAmount]) {
-      commit('changeAmountFromCartById', [phone_id, newAmount])
+    changeAmountFromCartById({commit}, [phone_id, new_amount]) {
+      commit('changeAmountFromCartById', [phone_id, new_amount])
     }
   },
   getter: {
