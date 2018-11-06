5463e36e6dd28c63fc45eebb45f301985f356d37
Vladislav Starikov <vlad.starikov.88@gmail.com>
2018-11-06 17:23:22 +0300

    Добавлен пользователь в store

--- front-end/src/store/user.js
+++ front-end/src/store/user.js
@@ -1,9 +1,16 @@
 const user = {
   state: {
-
+    access_token: null
   },
   mutations: {
-    
+    setAccessToken(state, access_token) {
+      state.access_token = access_token
+    }
+  },
+  actions: {
+    setAccessToken({commit}, access_token) {
+      commit('setAccessToken', access_token)
+    }
   }
 }
 
