cfa6f592c73c4c043644cceef64da7aab7c75dce
Vladislav Starikov <vlad.starikov.88@gmail.com>
2018-11-06 19:04:41 +0300

    Добавлены мутации и экшены для cart.js

--- front-end/src/store/cart.js
+++ front-end/src/store/cart.js
@@ -17,6 +17,13 @@ const cart = {
     },
     removeFromCartById(state, phone_id) {
       lodash.remove(state.cart, {phone_id})
+    },
+    changeAmountFromCartById(state, [phone_id, newAmount]) {
+      const current = lodash.find(state.cart, {phone_id}) 
+
+      if (current) {
+        current.amount = newAmount
+      }
     }
   },
   actions: {
@@ -25,6 +32,9 @@ const cart = {
     },
     removeFromCartById({commit}, phone_id) {
       commit('removeFromCartById', phone_id)
+    },
+    changeAmountFromCartById({commit}, [phone_id, newAmount]) {
+      commit('changeAmountFromCartById', [phone_id, newAmount])
     }
   },
   getter: {
