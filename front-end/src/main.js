10e9426e4f18a9643dbf6011461022f417c41742
Vladislav Starikov <vlad.starikov.88@gmail.com>
2018-11-06 15:53:48 +0300

    store

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from '@/App.vue'
 import lodash from 'lodash'
 import store from '@/store/index'
 
 Vue.set(Vue.prototype, 'lodash', lodash)
 Vue.config.productionTip = false
-Vue.use(Vuex);
 
 new Vue({
   store,
--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -1,6 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
 import createLogger from 'vuex/dist/logger'
 import createPersistedState from 'vuex-persistedstate'
 
+
+import cart from './cart'
+import wishlist from './wishlist'
+import user from './user'
+
+Vue.use(Vuex);
 const store = new Vuex.Store({
     plugins: [createLogger(), createPersistedState()],
     modules: {
