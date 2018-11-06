4f4a6fac1e4986d4fd11e9d60dc06b42e4c486fa
Vladislav Starikov <vlad.starikov.88@gmail.com>
2018-11-06 14:51:32 +0300

    add bulma

---
+++ front-end/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    css: {
+      loaderOptions: {
+        // передача настроек в sass-loader
+        sass: {
+          // @/ это псевдоним к каталогу src/ поэтому предполагается,
+          // что у вас в проекте есть файл `src/variables.scss`
+          data: `@import "bulma";`
+        }
+      }
+    }
+  }
