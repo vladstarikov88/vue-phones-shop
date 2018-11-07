27479fbd75de22a8973188b2498148f19f31836e
Sergey <s.cholostov@wis.software>
2018-11-07 15:33:00 +0300

    fa

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -4,6 +4,13 @@ import lodash from 'lodash'
 import router from '@/router'
 import store from '@/store/index'
 import axiosMock from '@/mock/data'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import icons from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(icons)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.set(Vue.prototype, 'lodash', lodash)
 Vue.config.productionTip = false
