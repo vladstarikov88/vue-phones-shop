9267fab8235543c39bbb7867b4b318ca37c3f310
Vladislav Starikov <vlad.starikov.88@gmail.com>
2018-11-07 16:53:00 +0300

    Модальное окно

---
+++ front-end/src/components/ModalWindow.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="modal" :class="{ 'is-active': isOpen}">
+        <div class="modal-backgroung">
+            <div class="modal-content">
+                <div class="box">
+                    <slot></slot> 
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ModalWindow",
+    props: ['isOpen'],
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
