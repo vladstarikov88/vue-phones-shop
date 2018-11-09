624c0df9be831500d2ad046e58bf1a010f2eca9a
Sergey <s.cholostov@wis.software>
2018-11-09 12:03:12 +0300

    init cart page

---
+++ front-end/src/pages/cart.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+
+  <section class="section">
+    Корзина
+  </section>
+
+</template>
+
+<script lang="js">
+  export default  {
+    name: 'cart',
+    props: [],
+    mounted() {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+  .cart {
+
+  }
+</style>
