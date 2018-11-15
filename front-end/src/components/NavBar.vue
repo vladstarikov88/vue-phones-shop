97f422374631bef912d69e95086ae71109e8648b
l.roskoshin <l.roskoshin@wis.software>
2018-11-15 18:24:52 +0300

    Логотип

--- front-end/src/components/NavBar.vue
+++ front-end/src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item" href="/">
-        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+        <strong>МАГАЗИН</strong>
       </a>
       <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
          data-target="navbarBasicExample">
