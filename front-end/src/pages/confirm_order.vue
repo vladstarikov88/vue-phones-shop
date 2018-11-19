9705725ea0624106b8b5d44cc9497f299604632c
v.starikov <v.starikov@wis.software>
2018-11-19 13:11:55 +0300

    Изменение страницы подтверждения

--- front-end/src/pages/confirm_order.vue
+++ front-end/src/pages/confirm_order.vue
@@ -3,48 +3,39 @@
         <div class="container flex">
             <h1 class="title">Оформить заказ</h1>
                 <div v-if="getAccessTocken">
-                    <div class="box">
-                        <article class="media">
-                            <div class="media-left">
-                            <figure class="image is-96x96">
-                                <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
-                            </figure>
-                            </div>
-                            <div class="media-content">
-                                <div class="content">
-                                    <p><strong>John Smith</strong> <small>@example.com</small></p>
-                                    <table class="table">
-                                        <tr>
-                                            <th>Адрес доставки: </th>
-                                            <th>Номер телефона</th>
-                                            <th>Редактировать</th>
-                                        </tr>
-                                        <tr>
-                                            <td>Ростов-на-Дону, ул. Красноармейская 210</td>
-                                            <td>+7(918) 321-45-71</td>
-                                            <td>
-                                                <div class="buttons">
-                                                    <a class="button is-warning"
-                                                    @click="editInfoInRow(purchase.id)">
-                                                    <span class="icon is-small">
-                                                        <i class="fa-edit" :class="[ is_editing ? 'fas': 'far']"></i>
-                                                    </span>
-                                                    </a>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </table>
-                                </div>
-                            </div>
-                        </article>
+                    <div class="content">
+                        <table class="table">
+                            <tr>
+                                <th>Адрес доставки: </th>
+                                <th>Получатель</th>
+                                <th>E-mail</th>
+                            </tr>
+                            <tr>
+                                <td>{{address}}</td>
+                                <td>{{username}}</td>
+                                <td>{{email}}</td>
+                            </tr>
+                        </table>
                     </div>
-                    <a href="" class="button is-link">Подтвердить и оплатить</a>
+                    <div class="field is-grouped">
+                        <div class="control">
+                            <button class="button is-link">Подтвердить и оплатить</button>
+                        </div>
+                        <div class="control">
+                            <button class="button is-text">Редактировать</button>
+                        </div>
+                    </div>
                 </div>
                 <div v-else>
                     <div class="form">
                         <div class="field">
                             <div class="control">
                                 <input class="input" type="text" placeholder="Имя">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="control">
+                                <input class="input" type="text" placeholder="Адрес доставки">
                             </div>
                         </div>
                         <div class="field">
@@ -55,7 +46,7 @@
                                 </span>
                             </div>
                         </div>
-                        <div class="field">
+                        <div class="field is-grouped">
                             <div class="control">
                                 <button class="button is-link">Подтвердить и оплатить</button>
                             </div>
@@ -71,10 +62,14 @@ import {mapGetters} from 'vuex'
 export default {
     data() {
         return {
-            is_editing: false
+            is_editing: false,
+            address: "Ростов-на-Дону, ул. Красноармейская 210",
+            username: "John Smith",
+            email: "john@example.com"
         }
     },
     components: {
+        
     },
     computed: {
         ...mapGetters('user', ['getAccessTocken']),
