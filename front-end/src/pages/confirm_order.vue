019d3404e904bf407ba0eb927f0b18bdc64bcd53
v.starikov <v.starikov@wis.software>
2018-11-19 16:52:32 +0300

    Отправка данных от неавторизованного пользователя

--- front-end/src/pages/confirm_order.vue
+++ front-end/src/pages/confirm_order.vue
@@ -25,26 +25,64 @@
             <div v-else> -->
                 <div class="form">
                     <div class="field">
-                        <div class="control">
-                            <input class="input" type="text" placeholder="Имя">
-                        </div>
-                    </div>
-                    <div class="field">
-                        <div class="control">
-                            <input class="input" type="text" placeholder="Адрес доставки">
+                        <div class="control has-icons-right">
+                            <input 
+                                class="input" 
+                                type="text" 
+                                placeholder="Имя"
+                                v-model="username"
+                                v-validate="'required|min:8'" 
+                                data-vv-as="field" 
+                                name="min_field">
+                            <span class="icon is-small is-right">
+                                <i class="fas fa-exclamation-triangle"></i>
+                            </span>
+                            <p
+                                class="help is-danger"
+                                v-if="errors.has('min_field')">
+                                Длина имени должна быть не менее 8 символов</p>
                         </div>
                     </div>
                     <div class="field">
                         <div class="control has-icons-right">
-                            <input class="input" type="email" placeholder="Email">
+                            <input 
+                                class="input" 
+                                type="text" 
+                                placeholder="Адрес доставки"
+                                v-model="address"
+                                v-validate="'required'"
+                                data-vv-as="field" >
                             <span class="icon is-small is-right">
                                 <i class="fas fa-exclamation-triangle"></i>
                             </span>
                         </div>
                     </div>
+                    <div class="field">
+                        <div class="control has-icons-right">
+                            <input 
+                                class="input" 
+                                type="email" 
+                                placeholder="Email"
+                                v-model="email"
+                                v-validate="'required|email'" 
+                                data-vv-as="email" 
+                                name="email_field"
+                                :class="{'is-danger' : errors.has('email_field')}">
+                            <span class="icon is-small is-right">
+                                <i class="fas fa-exclamation-triangle"></i>
+                            </span>
+                            <p 
+                                class="help is-danger" 
+                                v-if="errors.has('email_field')">
+                                Неверный формат</p>
+                        </div>
+                    </div>
                     <div class="field is-grouped">
                         <div class="control">
-                            <button class="button is-link">Подтвердить и оплатить</button>
+                            <button 
+                                class="button is-link"
+                                :disabled="errors.items.length > 0"
+                                @click="sendForm">Подтвердить и оплатить</button>
                         </div>
                     </div>
                 </div>
@@ -60,19 +98,23 @@
 <script>
 import AddressInfo from '@/components/AddressInfo'
 import ModalAddAddress from '@/components/modal/ModalAddAddress'
-import {mapGetters} from 'vuex'
+import {mapGetters, mapState} from 'vuex'
 export default {
     data() {
         return {
             modal_address_is_open: false, 
-            addresses: [
-                {
-                    id: 0,
-                    address: "Ростов-на-Дону, ул. Красноармейская 210",
-                    username: "John Smith",
-                    email: "john@example.com"
-                }
-            ]
+            username: null, 
+            address: null,
+            email: null
+            // addresses: [
+            //     {
+            //         id: 0,
+            //         address: "Ростов-на-Дону, ул. Красноармейская 210",
+            //         username: "John Smith",
+            //         email: "john@example.com"
+            //     }
+            // ]
+
         }
     },
     components: {
@@ -81,7 +123,7 @@ export default {
     },
     computed: {
         ...mapGetters('user', ['getAccessTocken']),
-        ...mapGetters('user', ['user']),
+        ...mapState('cart', ['cart'])
     },
     methods: {
         openModal() {
@@ -93,6 +135,26 @@ export default {
         addNewAddress(new_address) {
             new_address.id = this.addresses.length;
             this.addresses.push(new_address)
+        },
+        sendForm(obj){
+            let user_data = {
+                username: this.username,
+                address: this.address, 
+                email: this.email,
+                products: this.cart 
+            };
+            user_data = JSON.parse(JSON.stringify(user_data));
+            console.log(user_data)
+
+            this.$validator.validateAll().then((result) => {
+                if (result) {
+                    this.$notify({
+                        message: "Заявка успешно отправлена",
+                        status: "info"
+                    });
+                return;
+                }
+            });
         }
     }
 }
