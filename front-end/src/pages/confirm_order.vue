9d13049cd07a04b8df12056a3fb86876ffadadef
v.starikov <v.starikov@wis.software>
2018-11-19 12:14:27 +0300

    Разделение компонентов

--- front-end/src/pages/confirm_order.vue
+++ front-end/src/pages/confirm_order.vue
@@ -1,41 +1,84 @@
 <template>
     <section class="section">
         <div class="container flex">
-            <div class="form">
-                <h1 class="title">Оформить заказ</h1>
-                <div class="field">
-                    <div class="control">
-                        <input class="input" type="text" placeholder="Имя">
+            <h1 class="title">Оформить заказ</h1>
+                <div v-if="getAccessTocken">
+                    <div class="box">
+                        <article class="media">
+                            <div class="media-left">
+                            <figure class="image is-96x96">
+                                <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+                            </figure>
+                            </div>
+                            <div class="media-content">
+                                <div class="content">
+                                    <p><strong>John Smith</strong> <small>@example.com</small></p>
+                                    <table class="table">
+                                        <tr>
+                                            <th>Адрес доставки: </th>
+                                            <th>Номер телефона</th>
+                                            <th>Редактировать</th>
+                                        </tr>
+                                        <tr>
+                                            <td>Ростов-на-Дону, ул. Красноармейская 210</td>
+                                            <td>+7(918) 321-45-71</td>
+                                            <td>
+                                                <div class="buttons">
+                                                    <a class="button is-warning"
+                                                    @click="editInfoInRow(purchase.id)">
+                                                    <span class="icon is-small">
+                                                        <i class="fa-edit" :class="[ is_editing ? 'fas': 'far']"></i>
+                                                    </span>
+                                                    </a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
+                        </article>
                     </div>
+                    <a href="" class="button is-link">Подтвердить и оплатить</a>
                 </div>
-                <div class="field">
-                    <div class="control has-icons-right">
-                        <input class="input" type="email" placeholder="Email">
-                        <span class="icon is-small is-right">
-                            <i class="fas fa-exclamation-triangle"></i>
-                        </span>
-                    </div>
-                </div>
-                <div class="field is-grouped">
-                    <div class="control">
-                        <button class="button is-link">Submit</button>
-                    </div>
-                    <div class="control">
-                        <button class="button is-text">Cancel</button>
+                <div v-else>
+                    <div class="form">
+                        <div class="field">
+                            <div class="control">
+                                <input class="input" type="text" placeholder="Имя">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="control has-icons-right">
+                                <input class="input" type="email" placeholder="Email">
+                                <span class="icon is-small is-right">
+                                    <i class="fas fa-exclamation-triangle"></i>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="control">
+                                <button class="button is-link">Подтвердить и оплатить</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div v-if="getAccessTocken">Вход выполнен</div>
-        <div v-else>Не выполнен</div>
     </section>
 </template>
 
 <script>
 import {mapGetters} from 'vuex'
 export default {
+    data() {
+        return {
+            is_editing: false
+        }
+    },
+    components: {
+    },
     computed: {
         ...mapGetters('user', ['getAccessTocken']),
+        ...mapGetters('user', ['user']),
     }
 }
 </script>
@@ -45,6 +88,6 @@ export default {
         text-align: center;
     }
     .form {
-        padding: 0 25vw;
+        margin: 0 25vw;
     }
 </style>
