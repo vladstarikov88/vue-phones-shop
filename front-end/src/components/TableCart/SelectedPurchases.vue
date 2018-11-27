40771ec4e0064f3582c18a303a651267b66ed107
l.roskoshin <l.roskoshin@wis.software>
2018-11-27 16:29:11 +0300

    fix

--- front-end/src/components/TableCart/SelectedPurchases.vue
+++ front-end/src/components/TableCart/SelectedPurchases.vue
@@ -1,24 +1,24 @@
 <template>
-    <table class="table is-fullwidth">
-        <tr>
-            <th>Миниатюра</th>
-            <th>Модель</th>
-            <th>Колличество</th>
-            <th>Стоимость</th>
-        </tr>
-        <tr v-for="purchase in purchases" :key="purchase.id">
-            <td>
-                <figure class="image is-64x64"><img :src="purchase.image_url" alt=""></figure>
-            </td>
-            <td>{{purchase.name}}</td>
-            <td>{{purchase.amount}}</td>
-            <td>{{purchase.amount * purchase.price}}</td>
-        </tr>
-    </table>
+  <table class="table is-fullwidth">
+    <tr>
+      <th>Миниатюра</th>
+      <th>Модель</th>
+      <th>Колличество</th>
+      <th>Стоимость</th>
+    </tr>
+    <tr :key="purchase.id" v-for="purchase in purchases">
+      <td>
+        <figure class="image is-64x64"><img :src="purchase.image_url" alt=""></figure>
+      </td>
+      <td>{{purchase.name}}</td>
+      <td>x {{purchase.amount}} шт.</td>
+      <td>{{purchase.amount * purchase.price}} руб.</td>
+    </tr>
+  </table>
 </template>
 
 <script>
-export default {
+  export default {
     props: ['purchases']
-}
+  }
 </script>
--- front-end/src/pages/cart.vue
+++ front-end/src/pages/cart.vue
@@ -1,14 +1,14 @@
 <template>
 
   <section class="section">
-    <div class="container" >
-      <div v-show="!loading" v-if="purchases && purchases.length">
+    <div class="container">
+      <div v-if="purchases && purchases.length" v-show="!loading">
         <table-cart :purchases="purchases"></table-cart>
         <router-link :disabled="!has_selected_purchases" class="button" tag="button" to="/confirm_order">
           Перейти к оформлению покупки
         </router-link>
       </div>
-      <p v-show="!loading" class="title is-5" v-else>В корзину пока ни чего не добавленно</p>
+      <p class="title is-5" v-else v-show="!loading">В корзину пока ни чего не добавленно</p>
       <loader v-show="loading"></loader>
     </div>
   </section>
@@ -20,6 +20,7 @@
   import TableCart from '@/components/TableCart'
   import {db} from '@/plugins/FirebasePlugin.js'
   import Loader from '@/components/Loader';
+
   export default {
     name: 'cart',
 
@@ -36,27 +37,27 @@
     asyncComputed: {
       purchases() {
         this.loading = true;
-        const phones_queries = this.lodash.map(this.cart, ({phone_id, amount, selected})=>{
+        const phones_queries = this.lodash.map(this.cart, ({phone_id, amount, selected}) => {
+
           return db.collection('phones')
-            .where('id','==', phone_id)
+            .doc(phone_id)
             .get()
-            .then( snapshot => {
-              return this.lodash.map(snapshot.docs, (doc) => {
-                if(doc.exists) {
-                  const phone  = doc.data();
-                  return {
-                    price: phone.price,
-                    name: phone.name,
-                    image_url: phone.image_url,
-                    id: phone_id,
-                    amount,
-                    selected,
-                  }
+            .then(doc => {
+              if (doc.exists) {
+                const phone = doc.data();
+                return {
+                  price: phone.price,
+                  name: phone.name,
+                  image_url: phone.image_url,
+                  id: phone_id,
+                  amount,
+                  selected,
                 }
-              });
+              }
+
             });
         });
-        return Promise.all(phones_queries).then(this.lodash.flatten).finally(()=>{
+        return Promise.all(phones_queries).finally(() => {
           this.loading = false;
         });
       }
