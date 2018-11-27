bffb7c0449bb015b0d4c15105057b502c603c4b8
v.starikov <v.starikov@wis.software>
2018-11-27 18:57:46 +0300

    fix cart. add watcher

--- front-end/src/pages/cart.vue
+++ front-end/src/pages/cart.vue
@@ -34,40 +34,91 @@
       Loader,
       TableCart
     },
-    asyncComputed: {
-      purchases() {
-        this.loading = true;
-        const phones_queries = this.lodash.map(this.cart, ({phone_id, amount, selected}) => {
-
+    methods: {
+      fetchPhones() {
+        const promises = this.lodash.map(this.cart, ({phone_id, amount, selected}) => {
+          this.loading = true
           return db.collection('phones')
-            .doc(phone_id)
-            .get()
-            .then(doc => {
-              if (doc.exists) {
-                const phone = doc.data();
-                return {
-                  price: phone.price,
-                  name: phone.name,
-                  image_url: phone.image_url,
-                  id: phone_id,
-                  amount,
-                  selected,
-                }
+          .doc(phone_id)
+          .get()
+          .then(doc => {
+            if(doc.exists){
+              const phone = doc.data()
+              return {
+                id: phone_id,
+                name: phone.name,
+                price: phone.price,
+                quantity: phone.quantity,
+                amount,
+                selected
               }
-
-            });
-        });
-        return Promise.all(phones_queries).finally(() => {
-          this.loading = false;
-        });
+            }
+          })
+        })
+        Promise.all(promises)
+        .then(data => this.phones = data)
+        .finally(() => this.loading = false)
       }
     },
+    // asyncComputed: {
+    //   purchases() {
+    //     const phones_queries = this.lodash.map(this.cart, ({phone_id, amount, selected}) => {
+
+    //       return db.collection('phones')
+    //         .doc(phone_id)
+    //         .get()
+    //         .then(doc => {
+    //           if (doc.exists) {
+    //             const phone = doc.data();
+    //             return {
+    //               price: phone.price,
+    //               name: phone.name,
+    //               image_url: phone.image_url,
+    //               id: phone_id,
+    //               amount,
+    //               selected,
+    //             }
+    //           }
+
+    //         });
+    //     });
+    //     return Promise.all(phones_queries)
+    //   }
+    // },
     computed: {
       ...mapState('cart', ['cart']),
       ...mapGetters('cart', ['getSelectedPhones']),
       has_selected_purchases() {
         return this.getSelectedPhones.length
       },
+      purchases() {
+        const raw_wishes = this.lodash.map(this.cart, ({phone_id, amount, selected}) => {
+          const phone = this.lodash.find(this.phones, {id: phone_id})
+          if(phone) {
+            return {
+              price: phone.price,
+              name: phone.name,
+              image_url: phone.image_url,
+              id: phone_id,
+              amount, 
+              selected
+            }
+          }
+          return null
+        })
+        return this.lodash.filter(raw_wishes)
+      }
     },
+    watch: {
+      cart: {
+        deep: true,
+        handler(new_val, old_val) {
+          if (new_val != old_val) {
+            this.fetchPhones()
+          }
+        },
+        immediate: true,
+      }
+    }
   }
 </script>
