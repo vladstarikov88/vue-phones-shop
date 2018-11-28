ed14a70a7df56c6b4353d8b98cf1ef6a8e74d65c
v.starikov <v.starikov@wis.software>
2018-11-28 09:58:43 +0300

    fix visible of quantity on modal-add-to-cart

--- front-end/src/pages/wishlist.vue
+++ front-end/src/pages/wishlist.vue
@@ -25,10 +25,11 @@
             </div>
             <p class="title is-5" v-else>Список желаний пуст</p>
             <modal-add-to-cart
-                v-if="modal_is_open"
-                v-on:close="closeModal()"
-                :key="current_wish.id"
-                :phone="current_wish">
+              v-if="modal_is_open"
+              v-on:close="closeModal()"
+              :key="current_wish.id"
+              :phone="current_wish"
+              @add-to-cart="addToCart">
             </modal-add-to-cart>
         </div>
         <loader v-show="loading"></loader>
@@ -37,7 +38,7 @@
 </template>
 <script>
 import moment from '@/plugins/moment'
-import {mapState} from 'vuex'
+import {mapState, mapActions} from 'vuex'
 import TableWishlistRow from '@/components/TableWishlistRow'
 import ModalAddToCart from '@/components/modal/ModalAddToCart'
 import {db} from '@/plugins/FirebasePlugin.js'
@@ -45,79 +46,88 @@ import Loader from '@/components/Loader';
 export default {
     name: 'wishlist',
     data() {
-        return {
-            phones: [],
-            modal_is_open: false,
-            current_wish: {},
-            loading: false
-        }
+      return {
+        phones: [],
+        modal_is_open: false,
+        current_wish: {},
+        loading: false
+      }
     },
     components: {
-        TableWishlistRow,
-        ModalAddToCart,
-        Loader
+      TableWishlistRow,
+      ModalAddToCart,
+      Loader
     },
     computed: {
-        ...mapState('wishlist', ['wishlist']),
-        wishes() {
-            const raw_wishes = this.lodash.map(this.wishlist, ({phone_id, date}) => {
-                const phone = this.lodash.find(this.phones, {id: phone_id})
+      ...mapState('wishlist', ['wishlist']),
+      wishes() {
+        const raw_wishes = this.lodash.map(this.wishlist, ({phone_id, date}) => {
+          const phone = this.lodash.find(this.phones, {id: phone_id})
 
-                if(phone) {
-                    return {
-                        price: phone.price,
-                        name: phone.name,
-                        image_url: phone.image_url,
-                        id: phone_id,
-                        date: moment(date*1000).fromNow()
-                    }
-                }
-                return null
-            })
-        return this.lodash.filter(raw_wishes)
-        }
+          if(phone) {
+            return {
+              price: phone.price,
+              name: phone.name,
+              image_url: phone.image_url,
+              id: phone_id,
+              date: moment(date*1000).fromNow(),
+              quantity: phone.quantity
+            }
+          }
+          return null
+        })
+      return this.lodash.filter(raw_wishes)
+      }
     },
     watch: {
-        wishlist: {
-            deep: true,
-            handler(new_val, old_val) {
-                if (new_val != old_val) {
-                    this.fetchPhones()
-                }
-            },
-            immediate: true,
-        }
+      wishlist: {
+        deep: true,
+        handler(new_val, old_val) {
+          if (new_val != old_val) {
+            this.fetchPhones()
+          }
+        },
+        immediate: true,
+      }
     },
     methods: {
-        fetchPhones() {
-            const promises = this.lodash.map(this.wishlist, ({phone_id}) => {
-                this.loading = true
-                return db.collection('phones')
-                .doc(phone_id)
-                .get()
-                .then(doc => {
-                    if(doc.exists){
-                        const phone = doc.data()
-                        return {
-                            id: phone_id,
-                            name: phone.name,
-                            price: phone.price,
-                            quantity: phone.quantity
-                        }
-                    }
-                })
-            })
-            Promise.all(promises)
-            .then(data => this.phones = data)
-            .finally(() => this.loading = false)
-        },
-        openModal(wish) {
-            this.modal_is_open = true;
-            this.current_wish = wish
-        },
-        closeModal() {
-            this.modal_is_open = false
-        }
+      ...mapActions('cart', ['addToCartById']),
+      fetchPhones() {
+        const promises = this.lodash.map(this.wishlist, ({phone_id}) => {
+          this.loading = true
+          return db.collection('phones')
+          .doc(phone_id)
+          .get()
+          .then(doc => {
+            if(doc.exists){
+              const phone = doc.data()
+              return {
+                id: phone_id,
+                name: phone.name,
+                price: phone.price,
+                quantity: phone.quantity
+              }
+            }
+          })
+        })
+        Promise.all(promises)
+        .then(data => this.phones = data)
+        .finally(() => this.loading = false)
+      },
+      openModal(wish) {
+        this.modal_is_open = true;
+        this.current_wish = wish
+      },
+      closeModal() {
+        this.modal_is_open = false
+      },
+      addToCart(id, current_amount) {
+        this.addToCartById([id, current_amount]);
+        this.$notify({
+          message: "Товар был успешно добавлен в корзину.",
+          status: "info"
+        });
+      }
     },
 }
 </script>
